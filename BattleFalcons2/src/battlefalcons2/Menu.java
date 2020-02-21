399a034142bb708410c27618574406242c58b34c
Farzin Adil <54300496+farzinadil@users.noreply.github.com>
2020-02-20 18:56:08 -0800

    Update Menu.java

--- BattleFalcons2/src/battlefalcons2/Menu.java
+++ BattleFalcons2/src/battlefalcons2/Menu.java
@@ -12,11 +12,14 @@ public class Menu {
     JButton startgame;
     JButton instrutions;
     JLabel logo;
+    java.awt.Rectangle bounds = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+
 
     public Menu(){
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         startmenu = new JFrame();
-        startmenu.setSize((int)(screenSize.width*0.5), (int)(screenSize.height*0.5));
+        startmenu.setResizable(false);
+        startmenu.setSize((int)(bounds.width*0.5), (int)(bounds.height*0.5));
+        startmenu.setLocation((int)(bounds.width*0.25), (int)(bounds.height*0.25) );
         startmenu.setLayout(null);
         startmenu.getContentPane().setBackground(Color.white);
         startmenu.setTitle("Battle Falcons");
@@ -25,28 +28,22 @@ public class Menu {
 
 
         startgame = new JButton("Start");
-        startgame.setBounds((int)(screenSize.width*0.12), (int)(screenSize.height*0.35), (int)(screenSize.width*0.1) , (int)(screenSize.height*0.1));
+        startgame.setBounds((int)(bounds.width*0.135), (int)(bounds.height*0.35), (int)(bounds.width*0.1) , (int)(bounds.height*0.1));
         startgame.setVisible(true);
         startmenu.add(startgame);
 
 
-        instrutions = new JButton("Instruction");
-        instrutions.setBounds((int)(screenSize.width*0.25), (int)(screenSize.height*0.35), (int)(screenSize.width*0.1) , (int)(screenSize.height*0.1));
+        instrutions = new JButton("Instructions");
+        instrutions.setBounds((int)(bounds.width*0.265), (int)(bounds.height*0.35), (int)(bounds.width*0.1) , (int)(bounds.height*0.1));
         instrutions.setVisible(true);
         startmenu.add(instrutions);
 
-        logo = new JLabel();
+
         ImageIcon logoic = new ImageIcon("src/PNG/Battle_Falcon-1.png_3.png");
-        logo.setIcon(logoic);
+        logo = new JLabel(logoic);
         startmenu.add(logo);
-        logo.setBounds((int)(screenSize.width*0.17), (int)(screenSize.height*0.03), (int)(screenSize.width*0.3) , (int)(screenSize.height*0.30));
+        logo.setBounds((int)(bounds.width*0.5*0.1875), 0, (int)(bounds.width*0.5*0.625), (int)(bounds.height*0.5*0.9804));
         logo.setVisible(true);
-
-
-
-
-
-
 
 
 
@@ -69,8 +66,8 @@ public class Menu {
                                 + "You use mana to play birds, which automatically get placed on the leftmost tile of your playing field.\n"
                                 + "Your birds and your opponent’s will fight according to their placement on the battlefield.\n"
                                 + "The bird with the higher attack wins and kills the other bird.\n"
-                                + "If a bird is not facing any other birds, it attacks the opponent directly and deals its damage to his health total."
-                );
+                                + "If a bird is not facing any other birds, it attacks the opponent directly and deals its damage to his health total.", "Instructions", 0, logoic);
+
             }
         });
     }
